3e93d2e12acd4cd6d690a1cfbb5830eaecf1d58b
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-10-21 15:44:53 +0530

    Full theme created

--- app/public/wp-content/themes/my-theme/index.php
+++ app/public/wp-content/themes/my-theme/index.php
@@ -1,11 +1,9 @@
 
-
-
 <?php
 get_header();
 ?>
 <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(images/library-hero.jpg)"></div>
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>)"></div>
       <div class="page-banner__content container t-center c-white">
         <h1 class="headline headline--large">Welcome!</h1>
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
@@ -76,7 +74,7 @@ get_header();
     <div class="hero-slider">
       <div data-glide-el="track" class="glide__track">
         <div class="glide__slides">
-          <div class="hero-slider__slide" style="background-image: url(images/bus.jpg)">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri( 'images/bus.jpg' )?>);">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
                 <h2 class="headline headline--medium t-center">Free Transportation</h2>
@@ -85,7 +83,7 @@ get_header();
               </div>
             </div>
           </div>
-          <div class="hero-slider__slide" style="background-image: url(images/apples.jpg)">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri( 'images/apples.jpg' ) ?>);">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
                 <h2 class="headline headline--medium t-center">An Apple a Day</h2>
@@ -94,7 +92,7 @@ get_header();
               </div>
             </div>
           </div>
-          <div class="hero-slider__slide" style="background-image: url(images/bread.jpg)">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/bread.jpg') ?>);">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
                 <h2 class="headline headline--medium t-center">Free Food</h2>
