277f93e9908afc192c63ec500c4665e28e2202fb
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-10-21 12:05:17 +0530

    Header & Footer

---
+++ app/public/wp-content/themes/my-theme/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php wp_head();?>
+</head>
+<body>
+    <h1>NotatMrp</h1 >
+
+</body>
+</html>
+    
--- app/public/wp-content/themes/my-theme/single.php
+++ app/public/wp-content/themes/my-theme/single.php
@@ -1,7 +1,10 @@
 <?php
+get_header();
 while (have_posts()) {
     the_post();?>
     <h1><?php the_title(); ?></h1>
     <?php the_content()?>
  <?php }
+
+get_footer();
 ?>
