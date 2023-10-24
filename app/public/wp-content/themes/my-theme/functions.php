e946c7e0bd806002ab7d62641ec60d63672c20e0
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-10-24 22:37:45 +0530

    Changing

--- app/public/wp-content/themes/my-theme/functions.php
+++ app/public/wp-content/themes/my-theme/functions.php
@@ -3,10 +3,12 @@
 
 function bestdealsinfo_files(){
     wp_enqueue_script( 'main-univ-js',get_theme_file_uri( '/build/index.js'),array('jquery'),'1.0',true );
+    wp_enqueue_script( 'main-univ-extra-js',get_theme_file_uri( '/src/index.js'),array('jquery'),'1.0',true );
     wp_enqueue_style('bestdealsinfo_main_styles',get_theme_file_uri( '/build/style-index.css' ));
     wp_enqueue_style('bestdealsinfo_extra_styles',get_theme_file_uri( '/build/index.css' ));
     wp_enqueue_style('custom-google-fonts','https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome','https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    
 }
 add_action('wp_enqueue_scripts','bestdealsinfo_files');
 
